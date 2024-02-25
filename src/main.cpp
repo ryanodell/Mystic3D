f620f427fa93f1646ee9ebf3900ae12ee18eb481
Ryan ODell <ryanodell2013@gmail.com>
2024-02-25 10:16:12 -0800

    Adedd cpp properties file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include "stdio.h"
+#include "../vendor/GLM/glm.hpp"
 
 int main() {
-
-    printf("Test");
+    glm::vec2 test = glm::vec2(0.0f, 0.5f);
+    test++;
+    int testInt = 5;
+    printf("Test\n");
+    if(testInt == 5) {
+        int anotherintforif = 2;
+        anotherintforif++;
+        printf("Its 5 lol");
+    } else {
+        int elseintforif = 2;
+        testInt = elseintforif;
+        printf("Its not 5 lol");
+    }
     return 0;
 }
