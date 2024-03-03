a35d0d6571b70b4c6c999c1aeca6a5d7794bf68d
Ryan ODell <ryanodell2013@gmail.com>
2024-03-03 15:20:51 -0800

    added pch (not hooked up yet, just files)

---
+++ src/pch.h
@@ -0,0 +1,10 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#include "shader.h"
+#include <iostream>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
